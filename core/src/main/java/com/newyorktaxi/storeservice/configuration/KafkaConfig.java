fb8591c18fe074df6a9f84dc36f724eb2c860a7f
Slim <slima4.u8@gmail.com>
2023-06-14 17:11:31 +0200

    rewrite kafka consumer configuration

--- core/src/main/java/com/newyorktaxi/storeservice/configuration/KafkaConfig.java
+++ core/src/main/java/com/newyorktaxi/storeservice/configuration/KafkaConfig.java
@@ -1,62 +1,35 @@
 package com.newyorktaxi.storeservice.configuration;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.apache.avro.specific.SpecificRecordBase;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
-@AllArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class KafkaConfig<K extends Serializable, V extends SpecificRecordBase> {
+@EnableKafka
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
+public class KafkaConfig {
 
     KafkaProperties kafkaProperties;
-    KafkaConsumerProperties kafkaConsumerProperties;
 
     @Bean
-    public Map<String, Object> consumerConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, kafkaConsumerProperties.getKeyDeserializer());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, kafkaConsumerProperties.getValueDeserializer());
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaConsumerProperties.getAutoOffsetReset());
-        props.put(kafkaProperties.getSchemaRegistryUrlKey(), kafkaProperties.getSchemaRegistryUrl());
-        props.put(kafkaConsumerProperties.getSpecificAvroReaderKey(), kafkaConsumerProperties.getSpecificAvroReader());
-        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, kafkaConsumerProperties.getSessionTimeoutMs());
-        props.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, kafkaConsumerProperties.getHeartbeatIntervalMs());
-        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, kafkaConsumerProperties.getMaxPollIntervalMs());
-        props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG,
-                kafkaConsumerProperties.getMaxPartitionFetchBytesDefault() *
-                        kafkaConsumerProperties.getMaxPartitionFetchBytesBoostFactor());
-        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, kafkaConsumerProperties.getMaxPollRecords());
-        return props;
-    }
-
-    @Bean
-    public ConsumerFactory<K, V> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-    }
-
-    @Bean
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<K, V>> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<K, V> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        factory.setBatchListener(kafkaConsumerProperties.getBatchListener());
-        factory.setConcurrency(kafkaConsumerProperties.getConcurrencyLevel());
-        factory.setAutoStartup(kafkaConsumerProperties.getAutoStartup());
-        factory.getContainerProperties().setPollTimeout(kafkaConsumerProperties.getPollTimeoutMs());
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory
+                .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.kafkaProperties.buildConsumerProperties())));
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
         return factory;
     }
 }
--- core/src/main/java/com/newyorktaxi/storeservice/gateway/kafka/TaxiMessageKafkaListener.java
+++ core/src/main/java/com/newyorktaxi/storeservice/gateway/kafka/TaxiMessageKafkaListener.java
@@ -1,24 +1,25 @@
 package com.newyorktaxi.storeservice.gateway.kafka;
+
 import com.newyorktaxi.avro.model.TaxiMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.AcknowledgingMessageListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Slf4j
 @Component
-public class TaxiMessageKafkaListener implements KafkaConsumer<TaxiMessage> {
+public class TaxiMessageKafkaListener implements AcknowledgingMessageListener<String, Object> {
 
     @Override
     @KafkaListener(id = "${kafka-consumer-config.taxi-message-group-id}",
             topics = "${kafka-consumer-config.taxi-message-topic}")
-    public void receive(List<TaxiMessage> messages, List<String> keys, List<Integer> partitions, List<Long> offsets) {
-        log.info("Received {} messages from Kafka", messages.size());
-
-        for (int i = 0; i < messages.size(); i++) {
-            log.info("Received message: {} with key: {} from partition: {} at offset: {}",
-                    messages.get(i), keys.get(i), partitions.get(i), offsets.get(i));
+    public void onMessage(ConsumerRecord<String, Object> record, Acknowledgment acknowledgment) {
+        if(record.value() instanceof TaxiMessage taxiMessage) {
+            log.info("Received {} messages from Kafka", taxiMessage);
         }
+
+        acknowledgment.acknowledge();
     }
 }
