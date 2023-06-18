d78886b8ce1f239f8b83bbb83c26d4658104c776
Slim <slima4.u8@gmail.com>
2023-06-18 12:09:28 +0200

    refactoring tests

--- core/src/main/java/com/newyorktaxi/storeservice/gateway/kafka/TaxiMessageKafkaListener.java
+++ core/src/main/java/com/newyorktaxi/storeservice/gateway/kafka/TaxiMessageKafkaListener.java
@@ -8,11 +8,12 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.retrytopic.TopicSuffixingStrategy;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
@@ -31,17 +32,17 @@ public class TaxiMessageKafkaListener {
                     multiplierExpression = "${kafka-consumer-config.taxi-message-retry-multiplier}"))
     @KafkaListener(id = "${kafka-consumer-config.taxi-message-group-id}",
             topics = "${kafka-consumer-config.taxi-message-topic}")
-    public void onMessage(ConsumerRecord<String, TaxiMessage> record) {
-        log.info("Received record: {}", record);
+    public void onMessage(TaxiMessage taxiMessage) {
+        log.info("Received record: {}", taxiMessage);
 
-        final TaxiTripParams taxiTrip = taxiTripMapper.toTaxiTripParams(record.value());
+        final TaxiTripParams taxiTrip = taxiTripMapper.toTaxiTripParams(taxiMessage);
         saveTaxiTripUseCase.execute(taxiTrip);
 
         log.info("Successfully saved {} to database", taxiTrip);
     }
 
     @DltHandler
-    public void dltHandler(ConsumerRecord<String, TaxiMessage> record) {
-        log.info("Received record from DLT: {}", record);
+    public void dltHandler(TaxiMessage taxiMessage, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        log.info("Received topic {} with record from DLT: {}", topic, taxiMessage);
     }
 }
--- core/src/test/java/intg/com/newyorktaxi/storeservice/gateway/kafka/TaxiMessageKafkaListenerTest.java
+++ core/src/test/java/intg/com/newyorktaxi/storeservice/gateway/kafka/TaxiMessageKafkaListenerTest.java
@@ -5,12 +5,11 @@ import com.newyorktaxi.avro.model.TaxiMessage;
 import com.newyorktaxi.storeservice.StoreServiceApplication;
 import com.newyorktaxi.storeservice.TestData;
 import com.newyorktaxi.storeservice.TestUtil;
+import com.newyorktaxi.storeservice.mapper.TaxiTripMapper;
 import com.newyorktaxi.storeservice.repository.TaxiTripRepository;
-import com.newyorktaxi.storeservice.usecase.impl.SaveTaxiTripUseCase;
 import lombok.AccessLevel;
 import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -33,7 +32,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
@@ -55,6 +54,11 @@ import static org.mockito.Mockito.verify;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 class TaxiMessageKafkaListenerTest {
 
+    @Value("${kafka-consumer-config.taxi-message-topic}")
+    String topic;
+    @Value("${kafka-consumer-config.taxi-message-topic-dlt}")
+    String topicDlt;
+
     @Autowired
     EmbeddedKafkaBroker embeddedKafkaBroker;
 
@@ -68,13 +72,13 @@ class TaxiMessageKafkaListenerTest {
     TaxiMessageKafkaListener taxiMessageKafkaListener;
 
     @SpyBean
-    SaveTaxiTripUseCase saveTaxiTripUseCase;
+    TaxiTripMapper taxiTripMapper;
 
     @Autowired
     TaxiTripRepository taxiTripRepository;
 
     @BeforeEach
-    void setUp(@Value("${kafka-consumer-config.taxi-message-topic}") String topic) {
+    void setUp() {
         endpointRegistry.getListenerContainers()
                 .forEach(container ->
                         ContainerTestUtils.waitForAssignment(container, embeddedKafkaBroker.getPartitionsPerTopic()));
@@ -85,7 +89,7 @@ class TaxiMessageKafkaListenerTest {
         WireMock.resetToDefault();
 
         TestUtil.registerSchema(1, topic, TaxiMessage.getClassSchema().toString());
-        TestUtil.registerSchema(2, topic + "-dlt", TaxiMessage.getClassSchema().toString());
+        TestUtil.registerSchema(2, topicDlt, TaxiMessage.getClassSchema().toString());
     }
 
     @AfterEach
@@ -94,7 +98,6 @@ class TaxiMessageKafkaListenerTest {
     }
 
     @Test
-    @SuppressWarnings("unchecked")
     @SneakyThrows
     @DisplayName("test on message")
     void testOnMessage() {
@@ -104,34 +107,45 @@ class TaxiMessageKafkaListenerTest {
         await().atMost(5, TimeUnit.SECONDS)
                 .pollDelay(100, TimeUnit.MILLISECONDS)
                 .untilAsserted(() -> assertThat(taxiTripRepository.count()).isEqualTo(1));
-        verify(taxiMessageKafkaListener).onMessage(any(ConsumerRecord.class));
+        verify(taxiMessageKafkaListener).onMessage(taxiMessage);
     }
 
     @Test
-    @SuppressWarnings("unchecked")
     @SneakyThrows
     @DisplayName("test dead letter topic")
     void testDeadLetterTopic() {
-        final TaxiMessage taxiMessage = TestData.buildTaxiMessage();
-        final ArgumentCaptor<ConsumerRecord<String, TaxiMessage>> recordCaptor = ArgumentCaptor.forClass(ConsumerRecord.class);
+        final TaxiMessage actualTaxiMessage = TestData.buildTaxiMessage();
+        final ArgumentCaptor<TaxiMessage> taxiMessageCaptor = ArgumentCaptor.forClass(TaxiMessage.class);
+        final ArgumentCaptor<String> topicDltCaptor = ArgumentCaptor.forClass(String.class);
         final CountDownLatch latch = new CountDownLatch(1);
 
-        doThrow(new RuntimeException("Test exception")).when(saveTaxiTripUseCase).execute(any());
+        doThrow(new RuntimeException("Test exception")).when(taxiTripMapper).toTaxiTripParams(actualTaxiMessage);
         doAnswer(invocation -> {
             latch.countDown();
             return null;
-        }).when(taxiMessageKafkaListener).dltHandler(recordCaptor.capture());
+        }).when(taxiMessageKafkaListener).dltHandler(taxiMessageCaptor.capture(), topicDltCaptor.capture());
 
-        kafkaTemplate.sendDefault(taxiMessage).get();
+        kafkaTemplate.sendDefault(actualTaxiMessage).get();
 
         boolean await = latch.await(5, TimeUnit.SECONDS);
 
-        assertThat(await).isTrue();
+        assertThat(await)
+                .as("awaiting for latch")
+                .isTrue();
 
-        verify(taxiMessageKafkaListener).onMessage(any(ConsumerRecord.class));
-        verify(taxiMessageKafkaListener).dltHandler(any(ConsumerRecord.class));
+        verify(taxiMessageKafkaListener).onMessage(actualTaxiMessage);
+        verify(taxiMessageKafkaListener).dltHandler(actualTaxiMessage, topicDlt);
 
-        final ConsumerRecord<String, TaxiMessage> capturedRecord = recordCaptor.getValue();
-        assertThat(capturedRecord.value()).isEqualTo(taxiMessage);
+        final TaxiMessage expectedTaxiMessage = taxiMessageCaptor.getValue();
+        final String expectedTopicDlt = topicDltCaptor.getValue();
+
+        assertAll(
+                () -> assertThat(expectedTaxiMessage)
+                        .as("expected taxi message")
+                        .isEqualTo(actualTaxiMessage),
+                () -> assertThat(expectedTopicDlt)
+                        .as("expected topic dlt")
+                        .isEqualTo(topicDlt)
+        );
     }
 }
